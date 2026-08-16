91e1bb564235516b77b093349e71c9be64d324df
Khushboosingh29 <93135311+Khushboosingh29@users.noreply.github.com>
2026-08-16 15:18:10 +0530

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.cpp
@@ -0,0 +1,39 @@
+#include <cstring>
+#include <climits>
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+int dp[1001][1001];
+bool solve(string &s,int i,int j){
+    if(i>=j)
+        return true;
+    if (dp[i][j] != -1) 
+        return dp[i][j];
+    if(s[i]==s[j])
+       return  dp[i][j]=solve(s,i+1,j-1);
+    return dp[i][j]=0;
+}
+    string longestPalindrome(string s) {
+        memset(dp,-1,sizeof(dp));
+        int n=s.size();
+        int maxl=INT_MIN;
+        int si=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(solve(s,i,j))
+                {
+                    if(j-i+1>maxl)
+                         {
+                            maxl=j-i+1;
+                            si=i;
+                         }
+                }
+
+            }
+        }
+        return s.substr(si,maxl);
+        
+    }
+};
